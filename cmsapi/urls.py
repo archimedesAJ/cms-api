c01c651589ee2d1b880f6db0944c6973fb24ae0c
archimedesAJ <abrahamabbeyjr456@gmail.com>
2025-08-02 20:04:03 +0000

    update my settings

--- cmsapi/urls.py
+++ cmsapi/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('members/', include("member.urls")),
@@ -29,3 +31,6 @@ urlpatterns = [
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs')
    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

51cd882b4a782fa520cb417d00f702099705f014
archimedesAJ <abrahamabbeyjr456@gmail.com>
2025-08-02 17:38:17 +0000

    modified the serializers

--- member/serializers.py
+++ member/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers;
 from .models import Member
 
 class MemberSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
+    image = serializers.ImageField(max_length=None, use_url=True, required=False, allow_null=True)
     class Meta:
         model = Member
         fields = ['id', 'member_no', 'title', 'full_name',
